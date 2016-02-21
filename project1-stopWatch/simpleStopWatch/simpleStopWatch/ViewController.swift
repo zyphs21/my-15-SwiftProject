a4b0c4f537bcd4cedc1427bff7bff352ec531f56
Hanson <Hanson@promote.cache-dns.local>
2016-02-21 15:51:19 +0800

    add stop watch code

--- project1-stopWatch/simpleStopWatch/simpleStopWatch/ViewController.swift
+++ project1-stopWatch/simpleStopWatch/simpleStopWatch/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lableTime: UILabel!
+    
+    var isCounting : Bool = false
+    
+    var timer : NSTimer = NSTimer()
+    
+    var countNumber : Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+    @IBAction func retSetCount(sender: AnyObject) {
+        timer.invalidate()
+        isCounting = false
+        countNumber = 0
+        lableTime.text = String(countNumber)
+    }
+
+    @IBAction func startCount(sender: AnyObject) {
+        if isCounting{
+            return
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("updateTime"),userInfo: nil, repeats: true)
+        
+        isCounting = true
+    }
+   
+    @IBAction func stopCount(sender: AnyObject) {
+        timer.invalidate()
+        isCounting = false
+    }
+    
+    func updateTime(){
+        countNumber = countNumber + 0.1
+        lableTime.text = String(countNumber)
+        //lableTime.text = String(format: "%.1f", countNumber)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
