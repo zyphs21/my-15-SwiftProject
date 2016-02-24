6b44c7152b80cbb1b3765dfb063c79bece317870
Hanson <Hanson@promote.cache-dns.local>
2016-02-25 00:45:04 +0800

    add new project custom font

---
+++ project2-customFont/customFont/customFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customFont
+//
+//  Created by Hanson on 16/2/24.
+//  Copyright © 2016年 hanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
