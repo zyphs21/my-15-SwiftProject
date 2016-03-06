cf669c68507a6d0d8d957c51219680aade32b6b7
Hanson <Hanson@promote.cache-dns.local>
2016-03-06 23:14:32 +0800

    add new project

---
+++ project4-snapchatPage/snapchatPage/snapchatPage/LeftView.swift
@@ -0,0 +1,36 @@
+//
+//  LeftView.swift
+//  snapchatPage
+//
+//  Created by Hanson on 16/3/6.
+//  Copyright © 2016年 hanson. All rights reserved.
+//
+
+import UIKit
+
+class LeftView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //把statusbar隐藏
+        UIApplication.sharedApplication().statusBarHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project4-snapchatPage/snapchatPage/snapchatPage/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  snapchatPage
+//
+//  Created by Hanson on 16/3/6.
+//  Copyright © 2016年 hanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        UIApplication.sharedApplication().statusBarHidden = true
+        
+        let leftView: LeftView = LeftView(nibName:"LeftView", bundle: nil)
+        let centerView: CameraView = CameraView(nibName:"CameraView", bundle: nil)
+        let rightView: RightView = RightView(nibName:"Right", bundle: nil)
+
+        self.addChildViewController(leftView)
+        self.ScrollView.addSubview(leftView.view)
+        leftView.didMoveToParentViewController(self)
+        
+        self.addChildViewController(centerView)
+        self.ScrollView.addSubview(centerView.view)
+        centerView.didMoveToParentViewController(self)
+        
+        self.addChildViewController(rightView)
+        self.ScrollView.addSubview(rightView.view)
+        rightView.didMoveToParentViewController(self)
+        
+        var centerViewFrame: CGRect = centerView.view.frame
+        centerViewFrame.origin.x = self.view.frame.width
+        centerView.view.frame = centerViewFrame
+        
+        var rightViewFrame: CGRect = rightView.view.frame
+        rightViewFrame.origin.x = 2 * self.view.frame.width
+        rightView.view.frame = rightViewFrame
+        
+        self.ScrollView.contentSize = CGSizeMake(self.view.frame.width * 3, self.view.frame.size.height)
+        
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
